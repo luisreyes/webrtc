b501acbc322cc38d59ad4d1401bd65285f7db234
Luis Reyes <luis@luisreyes.com>
2015-07-25 02:11:59 -0400

    Adding FavIcon

--- config/express.js
+++ config/express.js
@@ -16,7 +16,7 @@ module.exports = function(app, config) {
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
 
-  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(favicon(config.root + '/public/img/favicon.png'));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
